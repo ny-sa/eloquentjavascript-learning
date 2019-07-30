9497d048f452d544b21f9e4d1235bc62e14682d2
ysan <msun698@gmail.com>
2019-07-30 09:50:22 -0700

    even more testing

--- Chapter 1/do_i_value_practice.js
+++ Chapter 1/do_i_value_practice.js
@@ -1,13 +1,17 @@
 let a = 2 ** 53;
 let b = 1 + 2 ** 53;
 let c = -3 + 2 ** 53;
-console.log('a = ' + a + "\nb = " + b + `\nc = ` + c); //test the limit
+//console.log('a = ' + a + "\nb = " + b + `\nc = ` + c); //test the limit
 //console.log(8888e-2+8888e2);
 //console.log(5/7);
-//console.log(1 - 2 + 3 / 4 * (3 + 2) ** 6);
+//console.log(1 - 2 + 3 / 4 * 5 ** (2+4)); //pemdas
 //console.log(Infinity);
 //console.log(Infinity -Infinity);
 a = 6;
+console.log(a);
 a += "pac";
 console.log(a);
-console.log(a - 3);
+console.log(a - 3); //strings and auto conversions
+console.log("Do you have a " + typeof 4.7 + "?" + "\nI'll give mine, no " + typeof "no way" + "s attached."); //not my type
+console.log(`7 to the power of 9 is ${7**9}.`);
+console.log('a'>'b');
