975c47d8689539143c629602a2c7bdf7493828fe
ysan <msun698@gmail.com>
2019-07-30 09:33:55 -0700

    more testing

--- Chapter 1/do_i_value_practice.js
+++ Chapter 1/do_i_value_practice.js
@@ -1,4 +1,13 @@
-var a = Math.pow(2,53);
-var b = 1 + Math.pow(2,53);
-var c = -3 + Math.pow(2,53);
+let a = 2 ** 53;
+let b = 1 + 2 ** 53;
+let c = -3 + 2 ** 53;
 console.log('a = ' + a + "\nb = " + b + `\nc = ` + c); //test the limit
+//console.log(8888e-2+8888e2);
+//console.log(5/7);
+//console.log(1 - 2 + 3 / 4 * (3 + 2) ** 6);
+//console.log(Infinity);
+//console.log(Infinity -Infinity);
+a = 6;
+a += "pac";
+console.log(a);
+console.log(a - 3);
