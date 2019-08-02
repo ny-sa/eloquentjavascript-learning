f6a6c1ae53348dd4661a407da4b3e8b9c9d180c6
ysan <msun698@gmail.com>
2019-08-01 18:40:19 -0700

    renaming

--- Chapter 2/2_Exercises.js
+++ Chapter 2/2_Exercises.js
@@ -24,13 +24,13 @@ for(let x = 1; x <= 100; x++){
 }
 console.log("----------------------------------");
 //Chessboard
-let size = 12;                           //Grid size
+let size = 16;                           //Grid size
 let polarity = true;                    //For even grid lengths, the pattern reverses each new line
 let result = "";
 for(let x = 1; x <= size**2; x++){
     if(x>size && x%size === 1){
         if(size%2 === 0){
-            polarity = !polarity;       //So this variable gets the program to flip the character printed every time it starts a new line
+            polarity = !polarity;       //This variable gets the program to flip the character printed every time it starts a new line
         }
             result+='\n';
     }
---
+++ Chapter 3/3_Exercises.js
@@ -0,0 +1,9 @@
+//No hints! Ever!
+//Minimum
+const min = (x, y) => {
+    if (x <= y)
+        return x;
+    else
+        return y;
+};
+console.log(min(1,1.5));
