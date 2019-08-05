5dace1a8f537a8fc2117116d0a3477dd1aa54b09
iisan <msun698@gmail.com>
2019-08-04 20:09:59 -0700

    slow day, small update

--- Chapter 4/i_do_not_object.js
+++ Chapter 4/i_do_not_object.js
@@ -8,3 +8,38 @@ const randomCombo = function() {                            //Now using arrays
 randomCombo();
 randomCombo();
 
+////////////////////Objects
+let abc = {
+    a: 'apple',
+    b: 'bird',
+    c: 'croissant'
+};
+console.log('That ' + abc.b + ' ate an ' + abc.a + '-flavored ' + abc.c + '.'); //Using properties
+let numbers = {a:5};
+Object.assign(numbers, {a:10582,b:82,c:9.283,d:(286/13)});              //Object.assign overwrites properties
+console.log(numbers);
+
+////////////////////Mutability
+let alpha = {a:3};
+let beta = alpha;
+Object.assign(alpha, {a:'A', b:'jumble', c:'of', d:'ideas'});
+console.log(beta);                                                          //beta is changed
+
+let gamma = 3;
+let delta = gamma;                                                          //a new 'object' is created with the same value
+gamma = ['A', 'jumble', 'of', 'ideas'];                                     //instead of just a reference
+console.log(gamma);
+console.log(delta);                                                         //delta is not changed
+
+////////////////////The lycanthrope’s log
+let journal = [];
+
+function addEntry(events, squirrel) {
+    journal.push({events, squirrel});
+}
+addEntry(["work", "touched tree", "pizza", "running",
+    "television"], false);
+addEntry(["work", "ice cream", "cauliflower", "lasagna",
+    "touched tree", "brushed teeth"], false);
+addEntry(["weekend", "cycling", "break", "peanuts",
+    "beer"], true);
