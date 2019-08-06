c28e43a90fc2cdb6723d6f30ba0841c35c7ae8f6
iisan <msun698@gmail.com>
2019-08-06 15:45:19 -0700

    one section

--- Chapter 4/i_do_not_object.js
+++ Chapter 4/i_do_not_object.js
@@ -26,20 +26,42 @@ Object.assign(alpha, {a:'A', b:'jumble', c:'of', d:'ideas'});
 console.log(beta);                                                          //beta is changed
 
 let gamma = 3;
-let delta = gamma;                                                          //a new 'object' is created with the same value
+let delta = gamma;                                                          //A new 'object' is created with the same value
 gamma = ['A', 'jumble', 'of', 'ideas'];                                     //instead of just a reference
 console.log(gamma);
 console.log(delta);                                                         //delta is not changed
 
-////////////////////The lycanthrope’s log
-let journal = [];
-
-function addEntry(events, squirrel) {
-    journal.push({events, squirrel});
+////////////////////Array Loops
+let list = [1, 2, 3, 4, 5], string1 = '';
+for (let x = 0; x < list.length; x++) {
+    string1 += list[x] + ' ';
 }
-addEntry(["work", "touched tree", "pizza", "running",
-    "television"], false);
-addEntry(["work", "ice cream", "cauliflower", "lasagna",
-    "touched tree", "brushed teeth"], false);
-addEntry(["weekend", "cycling", "break", "peanuts",
-    "beer"], true);
+console.log(string1); string1 = '';
+for (let x of list) {
+    string1 += x + ' ';
+}
+console.log(string1);                                                       //The result is the same
+
+////////////////////Further Arrayology
+const switchAround = front => {                                                 // Selects the first and last sections of an array,
+    return (back, array) => {                                                   // and switches them around, original order intact
+        if (front + back > array.length) {return 'Your array is too short!';}   // outer function takes in the first section length
+        let fArray = [], bArray = [];                                           // inner function takes in the last, along with the array
+        for (let a = front; a > 0; a--) { fArray.push(array.shift()); }
+        for (let b = back; b > 0; b--) { bArray.push(array.pop()); }
+        while (fArray.length !== 0) { array.push(fArray.shift()); }
+        while (bArray.length !== 0) { array.unshift(bArray.shift()); }
+        return array;
+    };
+};
+let num = [1,2,3,4,5,6];
+let first3 = switchAround(3);
+console.log(first3(2,num));
+num = [1,2,3];
+console.log(first3(2,num));
+let blah = '';
+num = first3(4, ['does', 'this', 'sentence', 'still', 'make', 'sense', 'after', 'the', 'switch?']);
+for (let x = 0; x < num.length; x++) {
+    blah += num[x] + ' ';
+}
+console.log(blah);                                                              //I don't quite get it
