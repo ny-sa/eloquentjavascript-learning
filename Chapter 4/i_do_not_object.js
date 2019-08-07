5df537fefc76029d480359129d14aa919b208002
iisan <msun698@gmail.com>
2019-08-06 19:19:36 -0700

    Finished Ch. 4 practice

--- Chapter 4/i_do_not_object.js
+++ Chapter 4/i_do_not_object.js
@@ -64,4 +64,65 @@ num = first3(4, ['does', 'this', 'sentence', 'still', 'make', 'sense', 'after', 
 for (let x = 0; x < num.length; x++) {
     blah += num[x] + ' ';
 }
-console.log(blah);                                                              //I don't quite get it
+console.log(blah);                                                              //I don't quite get you
+
+////////////////////Strings and their Properties
+function shatterScrambleStutter(victim) {
+    if (typeof victim !== 'string') {return 'Only strings are to be punished.'}
+    let pieces = [], start = 0;
+    for(let i = 0; i < victim.length; i++) {                                        //Shatters: Slices string at random lengths, averaging 3 characters each
+        if (Math.ceil(Math.random() * 3) === 1 || i === victim.length - 1) {
+            pieces.push(victim.slice(start, i+1));
+            start = i + 1;
+        }
+    }
+    let s, t, u, r;
+    for(let j = pieces.length - 1; j > 1; j--) {                                    //Scrambles: Shuffles sliced sections of the string
+        s = Math.floor((Math.random() * j) + 1);
+        t = pieces[j]; pieces[j] = pieces[s]; pieces[s] = t;
+    }
+    for(let k = 0; k < pieces.length; k++) {                                        //Stutters: 50% chance to add one extra stutter on top of another
+        u = 0; r = pieces[k];                                                       //Averages to one extra repeat for each segment
+        do {u++;} while(Math.floor(Math.random() * 2) !== 0);
+        pieces[k] = r.repeat(u);
+    }
+    return pieces.join('');                                                         //Joins the pieces together into a string
+}
+console.log(shatterScrambleStutter([3,6,9]));
+console.log(shatterScrambleStutter('I love the outdoors! Do you?'));
+
+////////////////////Rest Parameters
+let birds = {
+    words : ['Caw', 'Cheep', 'Chirp', 'Cluck', 'Coo', 'Cuckoo', 'Gobble gobble', 'Honk honk', 'Quack', 'Squawk', 'Tweet tweet', 'Warble'],
+    noises : function(b) {
+        let stringy = '';
+        while(b > 0) {
+            stringy += birds.words[Math.floor(Math.random() * birds.words.length)] + '!';
+            b--;
+            if (b > 0) stringy += ' ';
+        }
+        return stringy + ' ';
+    },
+    noise : (a, ...conversation) => {                                               //Bird noises are making it hard to hear
+        if (a < 0 || !Number.isInteger(a)) {                                        //a = noise amount
+            console.log('Positive integers only');                                  //...conversation = what is being interrupted
+            return;
+        }
+        let stringier = '';
+        for (let b = 0; b < conversation.length; b++) {
+            stringier += birds.noises(a) + '(' + conversation[b] + ') ' + birds.noises(a);
+            if (b < conversation.length - 1) stringier += '\n';
+        }
+        console.log(stringier);
+    }
+};
+birds.noise(4, 'I', "can't", 'hear', 'you!');
+birds.noise( 1, "They're quieter", 'now.');
+
+////////////////////The Math Object
+function thirdSideSAS(a, b, degree) {                                              //Finds third side given the first two and the angle they make
+    let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) - (2 * a * b * Math.cos(degree * Math.PI / 180)));
+    console.log('Given that side a equals ' + a + ', side b equals ' + b + ', and they make an angle of ' + degree + ' degrees,' + '\nside c roughly equals ' + c.toFixed(3));
+}
+thirdSideSAS(4,13, 102);
+
