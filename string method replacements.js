a5ebe98ed4a5097bc2631eb640cadf116f08c610
niisa <msun698@gmail.com>
2019-08-22 13:25:23 -0700

    changed toLowercase() and toUpperCase()

--- string method replacements.js
+++ string method replacements.js
@@ -37,9 +37,7 @@ function strRepeat(string, count) {
 function strSplit(string, separator) {
     let array = [], str = '';
     for(let i = 0, j = 0; i < string.length; i++) {
-        if (string[i] === separator || separator === '') {
-            array[j] = str; j++; i++; str = '';
-        }
+        if (string[i] === separator || separator === '') array[j] = str; j++; i++; str = '';
         str += string[i];
     }
     array[array.length] = str;
@@ -47,45 +45,46 @@ function strSplit(string, separator) {
 }
 function strSubstr(string, start, length) {
     let str = '';
-    for (let i = start, j = 0; j < length && string[i] !== undefined; i++, j++) {
-        str += string[i];
-    }
+    for (let i = start, j = 0; j < length && string[i] !== undefined; i++, j++) str += string[i];
     return str;
 }
 function strSubstring(string, start, end) {
     let str = '';
-    for (let i = start; i < end && string[i] !== undefined; i++) {
-        str += string[i];
-    }
+    for (let i = start; i < end && string[i] !== undefined; i++) str += string[i];
     return str;
 }
 function strToLowerCase(string) {
-    let str = '';
+    let str = '', already = false;
     let lower = 'abcdefghijklmnopqrstuvwxyz', upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     for (let i in string) {
-        if (strIncludes(upper, `${string[i]}`)) {
-            str += lower[strIndexOf(upper, string[i])];
-        }
-        else str += string[i];
+        already = false;
+        for (let j in upper)
+            if (string[i] == upper[j]) {
+                str += lower[j];
+                already = true;
+                break;
+            }
+        if (!already) str+= string[i];
     }
     return str;
 }
 function strToUpperCase(string) {
-    let str = '';
+    let str = '', already = false;
     let lower = 'abcdefghijklmnopqrstuvwxyz', upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     for (let i in string) {
-        if (strIncludes(lower, `${string[i]}`)) {
-            str += upper[strIndexOf(lower, string[i])];
-        }
-        else str += string[i];
+        already = false;
+        for (let j in lower)
+            if (string[i] == lower[j]) {
+                str += upper[j];
+                already = true;
+                break;
+            }
+        if (!already) str+= string[i];
     }
     return str;
 }
 function strTrim(string) {
     let str = '';
-    for (let i in string) {
-        if (string[i] !== ' ') str += string[i];
-    }
+    for (let i in string) if (string[i] !== ' ') str += string[i];
     return str;
 }
-
